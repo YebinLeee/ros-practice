eafe9df200bb3bd521505becc082d165eb978af1
Yebin <lyb2325@gmail.com>
2022-01-27 02:08:24 +0900

    ros2 launch

--- launch/arithmetic.launch.py
+++ launch/arithmetic.launch.py
@@ -21,32 +21,41 @@ from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
-
 def generate_launch_description():
-    param_dir = LaunchConfiguration(
+    # LaunchConfiguration 클래스 이용하여 실행 관련 설정을 선언
+    param_dir = LaunchConfiguration(   
         'param_dir',
         default=os.path.join(
+            # topic_~ 패키지의 param 폴더에 위치한 yaml 파라미터 설정파일
             get_package_share_directory('topic_service_action_rclcpp_example'),
             'param',
             'arithmetic_config.yaml'))
 
+    # 메소드의 리턴값으로 LaunchDescription 반환
     return LaunchDescription([
-        DeclareLaunchArgument(
+        DeclareLaunchArgument( 
             'param_dir',
             default_value=param_dir,
             description='Full path of parameter file'),
-
+        # 실행할 노드 설정
         Node(
-            package='topic_service_action_rclcpp_example',
-            executable='argument',
-            name='argument',
-            parameters=[param_dir],
-            output='screen'),
-
+            package='topic_service_action_rclcpp_example',  # 실행할 패키지 이름
+            executable='argument',                          # 실행 가능한 노드의 이름
+            name='argument',                                # 지정한 노드를 실행할 때 실제로 사용할 이름
+            parameters=[param_dir],                         # yaml 파일 사용
+            output='screen'),                               # screen: 로깅 정보가 기록되고 터미널 창에도 표시
         Node(
             package='topic_service_action_rclcpp_example',
             executable='calculator',
             name='calculator',
             parameters=[param_dir],
             output='screen'),
+        # remappings 기능 - 특정 이름 변경
+        Node(
+            package='topic_service_action_rclpy_example',
+            executable='argument',
+            name='argument',
+            remappings=[
+                ('/arithmetic_argument', '/argument'), # /arithmetic_argument 토픽 이름을 /argument 로 변경
+            ])
     ])
